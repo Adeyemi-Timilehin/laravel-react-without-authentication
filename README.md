57e2f825a1f40ecdb692546384f4c36f015808a7
Hadeymike <adeyemitimmy@gmail.com>
2022-02-09 14:21:33 +0100

    Starter kit for laravel react project without authentication

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a laravel-react raw file without authentication(breeze) just a starter kit 
+
+The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ resources/js/Pages/welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Welcome () {
+    return (
+        <>
+            <div>Hello Inertia!</div>
+            <h1>I love coding</h1>
+            <h1>I coding</h1>
+        </>
+    );
+}
